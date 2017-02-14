e0f380ab1610c7092b94ffb49867d6c249721bde
Abdullah P Rashed Ahmed <apra@slac.stanford.edu>
2017-02-13 16:38:27 -0800

    Adding the skywalker script

---
+++ skywalker.py
@@ -0,0 +1,14 @@
+################################################################################
+#                                  Skywalker                                   #
+################################################################################
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+from psp import Pv
+from joblib import Memory
+from beamDetector import Detector
+from walker import IterWalk
+
