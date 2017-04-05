3cf3e7fa45779bb9166a20fa5b53ba780986d0c9
apra93 <dd.apra93@gmail.com>
2017-04-05 09:14:16 -0700

    Adding files for last larger components of skywalker.

---
+++ pswalker/iterwalk.py
@@ -0,0 +1,11 @@
+# Iterwalk Module for Skywalker
+
+
+class IterWalker(object):
+	"""
+	IterWalker class that integrates with walker to perform a sequence of closed
+	loop moves to reach the goal pixel at each imager.
+	"""
+
+	def __init__(self):
+		pass
