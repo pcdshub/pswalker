664a00c4f6f372ff9861eb7a58efea2e8e1a2926
apra93 <dd.apra93@gmail.com>
2017-04-05 12:16:46 -0700

    Adding a model for the xrt simulation

---
+++ pswalker/models/xrt_model.py
@@ -0,0 +1,23 @@
+# Model used for the XRT Ray tracing simulator
+
+from modelwalk import ModelWalker
+from model_two_flat_tilted_mirrors_two_imagers import (
+	TwoFlatTiltedMirrorsTwoImagers)
+
+def get_model():
+    m = TwoFlatTiltedMirrorsTwoImagers()
+    m.source.x = 0.0
+    m.source.xp = 0.0
+    m.mirror_1.z = 90.510
+    m.mirror_2.z = 101.843
+    m.imager_1.z = 103.660
+    m.imager_2.z = 375.000
+    m.mirror_1.x = 0.0
+    m.mirror_2.x = 0.0317324
+    m.imager_1.x = 0.0317324
+    m.imager_2.x = 0.0317324
+    m.imager_1.image_xsz = 500
+    m.imager_1.mppix = 0.008/500
+    m.imager_2.image_xsz = 500
+    m.imager_2.mppix = 0.008/500
+    return m
