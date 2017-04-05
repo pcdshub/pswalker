a163334802ac23c67f45e87e4e3c3b34d44acc53
apra93 <dd.apra93@gmail.com>
2017-04-04 19:01:32 -0700

    Tested model walker using the analytical solution and it works. Will use that in the final version

--- pswalker/walker/iterwalk.py
+++ pswalker/walker/iterwalk.py
@@ -43,12 +43,15 @@ class IterWalker(object):
             p2 = np.array(p2)
         return np.linalg.norm(p2-p1)
 
-    def _alpha_1_calc(self):
+    def _alpha_1_calc(self, s=False):
         # self.goal_x_1 = self.imager_1.x - (self.p1 - ((
         #     self.imager_1.image_xsz+1)/2-1))*self.imager_1.mppix
         return alpha1(self.source.x, self.source.xp, self._alpha_2, 
                       self.mirror_1.z, self.mirror_2.z, self.imager_1.z,
-                      self.mirror_1.x, self.mirror_2.x, self.goal_x_1, t=self.tan)
+                      self.imager_2.z,
+                      self.mirror_1.x, self.mirror_2.x, self.goal_x_1, 
+                      self.goal_x_2, t=self.tan,
+                      s=s)
 
     def _alpha_2_calc(self):
         # self.goal_x_2 = self.imager_2.x - (self.p2 - ((
@@ -109,6 +112,15 @@ class IterWalker(object):
             self._move_mirrors(self._alpha_1, self._alpha_2)
         return self._alpha_1, self._alpha_2
 
+    def align2(self, p1=None, p2=None, move=False):
+        if p1 is not None: self.p1 = p1
+        if p2 is not None: self.p2 = p2
+        self._alpha_1 = self._alpha_1_calc(s=True)
+        self._alpha_2 = self._alpha_2_calc()
+        if move is True:
+            self._move_mirrors(self._alpha_1, self._alpha_2)
+        return self._alpha_1, self._alpha_2
+        
     @property
     def d1(self):
         return self._get_d(self.imager_1, self.p1)
@@ -133,16 +145,11 @@ class IterWalker(object):
 # x0, xp0, a1, a2, d2, d4, d5, d6, m1hdx, m2hdx, x, = sp.symbols(
 # 	"x0 xp0 a1 a2 d2 d4 d5 d6 m1hdx m2hdx x")
 
-def alpha1(x0, xp0, a2, d2, d4, d5, m1hdx, m2hdx, x, t=False):
+def alpha1(x0, xp0, a2, d2, d4, d5, d6, m1hdx, m2hdx, p1, p2, t=False, s=False):
     if t is False:        
-        return ((-a2*d2 + 4*a2*d4 - 3*a2*d5 + 2*d4*xp0 - 2*d5*xp0 - m2hdx + x - 
-                sqrt(a2**2*d2**2 - 8*a2**2*d2*d4 + 6*a2**2*d2*d5 + 
-                     8*a2**2*d4**2 - 8*a2**2*d4*d5 + a2**2*d5**2 - 
-                     4*a2*d2*d4*xp0 + 4*a2*d2*d5*xp0 + 2*a2*d2*m2hdx - 
-                     2*a2*d2*x + 8*a2*d4*m1hdx - 8*a2*d4*m2hdx + 4*a2*d4*x - 
-                     4*a2*d4*x0 - 8*a2*d5*m1hdx + 6*a2*d5*m2hdx - 2*a2*d5*x + 
-                     4*a2*d5*x0 + m2hdx**2 - 2*m2hdx*x + x**2)) / 
-                (4*(d4 - d5)))
+        return (-d4*d5*xp0 + d4*d6*xp0 - d4*p2 + d4*p1 + 2*d5*m1hdx - 2*d5*m2hdx - d5*x0 + d5*p2 - 2*d6*m1hdx + 2*d6*m2hdx + d6*x0 - d6*p1)/(2*(d2*d5 - d2*d6 - d4*d5 + d4*d6))
+    elif s is True:
+        return (-d4*xp0/2 + m1hdx - m2hdx + x/2 - x0/2)/(d2 - d4)
     elif t is True:
         a1 = sp.symbols("a1")
         return sp.nsolve(
@@ -160,21 +167,7 @@ def alpha1(x0, xp0, a2, d2, d4, d5, m1hdx, m2hdx, x, t=False):
             
 def alpha2(x0, xp0, a1, d2, d4, d6, m1hdx, m2hdx, x, t=False):
     if t is False:
-        return ((-2*a1*d2 + 8*a1*d4 - 6*a1*d6 - 4*d4*xp0 + 3*d6*xp0 + 2*m1hdx - 
-                 x - x0 + sqrt(
-                     4*a1**2*d2**2 - 32*a1**2*d2*d4 + 24*a1**2*d2*d6 + 
-                     32*a1**2*d4**2 - 32*a1**2*d4*d6 + 4*a1**2*d6**2 + 
-                     16*a1*d2*d4*xp0 - 12*a1*d2*d6*xp0 - 8*a1*d2*m1hdx + 
-                     4*a1*d2*x +  4*a1*d2*x0 - 32*a1*d4**2*xp0 + 
-                     32*a1*d4*d6*xp0 + 32*a1*d4*m1hdx - 16*a1*d4*m2hdx - 
-                     16*a1*d4*x0 - 4*a1*d6**2*xp0 - 24*a1*d6*m1hdx + 
-                     16*a1*d6*m2hdx - 4*a1*d6*x + 12*a1*d6*x0 + 8*d4**2*xp0**2 - 
-                     8*d4*d6*xp0**2 - 16*d4*m1hdx*xp0 + 8*d4*m2hdx*xp0 + 
-                     8*d4*x0*xp0 + d6**2*xp0**2 + 12*d6*m1hdx*xp0 - 
-                     8*d6*m2hdx*xp0 + 2*d6*x*xp0 - 6*d6*x0*xp0 + 4*m1hdx**2 - 
-                     4*m1hdx*x - 4*m1hdx*x0 + x**2 + 2*x*x0 + x0**2)) /
-                (4*(d4 - d6)))
-
+        return (a1*d2 - a1*d6 + d6*xp0/2 - m1hdx + m2hdx - x/2 + x0/2)/(d4 - d6)
     elif t is True:
         a2 = sp.symbols("a2")
         return sp.nsolve(
