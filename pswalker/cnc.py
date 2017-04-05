ef90dc2995239e8a614e14d2081247b442e7a118
apra93 <dd.apra93@gmail.com>
2017-04-05 08:58:06 -0700

    Adding cnc module

---
+++ pswalker/cnc.py
@@ -0,0 +1,15 @@
+# Command and Control Module for Skywalker
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+class CNC(object):
+	"""
+	Command and control class that interacts with the user and performs the
+	alignment.
+	"""
+	pass
+	
+	
