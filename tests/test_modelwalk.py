08261558d033323b10868b032670a50ef6784495
apra93 <dd.apra93@gmail.com>
2017-04-05 12:18:19 -0700

    Moving scaffolding code into testing like it should have been from the start. Made some changes to allow for testing using pytest.

--- tests/test_modelwalk.py
+++ tests/test_modelwalk.py
@@ -1,18 +1,15 @@
-# Scaffold module to write hard coded module classes for the purpose of testing 
-# while skywalker is incomplete. It is fully expected that this will disappear
-# once each of the components are fleshed out enough that the system can run.
+# Testing suite for modelwalk
 
-from __future__ import print_function
-
-from modelwalk import ModelWalker
-from models.templates.model_two_flat_tilted_mirrors_two_imagers import TwoFlatTiltedMirrorsTwoImagers
+from pswalker.modelwalk import ModelWalker
+from pswalker.models.templates.model_two_flat_tilted_mirrors_two_imagers import (
+    TwoFlatTiltedMirrorsTwoImagers)
 
 class Walker(object):
     def move_alpha_1(self, alpha):
         print("Moved alpha 1 motor to {0}".format(alpha))
     def move_alpha_2(self, alpha):
         print("Moved alpha 2 motor to {0}".format(alpha))
-    
+
 def get_model(p1, p2):
     m = TwoFlatTiltedMirrorsTwoImagers()
     m.source.x = 0.0
@@ -25,29 +22,37 @@ def get_model(p1, p2):
     m.mirror_2.x = 0.0317324
     m.imager_1.x = 0.0317324
     m.imager_2.x = 0.0317324
-    m.p1 = p1
-    m.p2 = p2
     m.imager_1.image_xsz = 500
     m.imager_1.mppix = 0.008/500
     m.imager_2.image_xsz = 500
     m.imager_2.mppix = 0.008/500
+    m.p1 = p1
+    m.p2 = p2
     return m
 
-def test_modelwalk():
+def test_algorithm_correctness_01():
     walker = Walker()
     model = get_model(250, 250)
-
     modWalker = ModelWalker(walker, model)
     alpha1, alpha2 = modWalker.align()
     assert(round(alpha1, 4) == 0.0014)
     assert(round(alpha2, 4) == 0.0014)
-    # except:
-    # import IPython; IPython.embed()
 
 
-if __name__ == "__main__":
-    test_modelwalk()
+def test_algorithm_correctness_02():
+    walker = Walker()
+    model = get_model(300, 400)
+    modWalker = ModelWalker(walker, model)
+    alpha1, alpha2 = modWalker.align()
+    assert(round(alpha1, 10) == round(0.0013644716418, 10))
+    assert(round(alpha2, 10) == round(0.00136741997231, 10))
 
-
+def test_algorithm_correctness_03():
+    walker = Walker()
+    model = get_model(180, 150)
+    modWalker = ModelWalker(walker, model)
+    alpha1, alpha2 = modWalker.align()
+    assert(round(alpha1, 10) == round(0.00144856550472, 10))
+    assert(round(alpha2, 10) == round(0.00144768100557, 10))
 
 
