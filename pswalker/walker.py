457cf2ec0a59b31f5a0d066d09c6960291c69902
apra93 <dd.apra93@gmail.com>
2017-04-09 00:40:20 -0700

    Added high level methods to move, move relative and jog to pixel. The actual mechanics of moving to an alpha and jogging an alpha need to be implemented.

--- pswalker/walker.py
+++ pswalker/walker.py
@@ -38,25 +38,58 @@ class Walker(object):
         self.imager_2 = kwargs.get("imager_2", Imager())
         self.p1 = kwargs.get("p1", 0)   #Desired point at imager 1
         self.p2 = kwargs.get("p2", 0)   #Desired point at imager 2
+        self.tolerance = kwargs.get("tolerance", 1e-6)
 
-    def move_alpha_1(self, new_alpha):
+    def _move_alpha(self, new_alpha, mirror):
         """Performs the necessary steps to do a move of mirror 1."""
-        # There are two objectives for a move made by walker:
-        # 1) Get to the goal
-        # 2) Archive data from the move
+        alpha = self.monitor.current_alphas[mirror]
+        # Only perform the move if it is to a position outside the tolerance
+        if new_alpha < alpha-self.tolerance or new_alpha > alpha+self.tolerance:
+            # There are two objectives for a move made by walker:
+            # 1) Get to the goal
+            # 2) Archive data from the move
+
+            # The first is simple. A proposal for the second would be to do a
+            # scan of n points between current_alpha1 and new_alpha1 and then at
+            # each point store alpha1, alpha2 and pixel position on the
+            # imager(s).
+
+            # This seems like a slight variation on some of the canonical
+            # bluesky scan demo so if that isn't what we are doing, why not?
+            raise NotImplementedError
+
+    def move_alphas(self, new_alpha_1, new_alpha_2):
+        """Moves mirrors 1 and 2 to the inputted pitches."""
+        self._move_alpha(new_alpha_1, 0)
+        self._move_alpha(new_alpha_2, 1)
+
+    def move_rel_alphas(self, rel_alpha_1, rel_alpha_2):
+        """Moves mirrors 1 and 2 by the inputted pitches."""
+        alphas = self.monitor.current_alphas
+        self._move_alpha(alphas[0] + rel_alpha_1, 0)
+        self._move_alpha(alphas[1] + rel_alpha_2, 1)
+
+    def _jog_alpha_to_pixel(self, new_pixel, mirror):
+        """
+        Jogs the inputted mirror pitch until the beam centroid reaches the
+        desired pixel.
+        """
+        current_centroid = self.monitor.current_centroids[mirror]
+        # Only perform the move if it is to a different pixel than where it is
+        if current_centroid != new_pixel:
+            # Determine which direction to jog
+            # Set up a monitor on the pv for beam centroidx
+            # Run twf or twr depending on direction until pv monitor reads the
+            # same centroid pixel as desired one
+            raise NotImplementedError
         
-        # The first is simple. A proposal for the second would be to do a scan
-        # of n points between current_alpha1 and new_alpha1 and then at each
-        # point store alpha1, alpha2 and pixel position on the imager(s).
-
-        # This seems like a slight variation on some of the canonical bluesky
-        # scan demo so if that isn't what we are doing, why not?
-        raise NotImplementedError
-
-    def move_alpha_2(self, new_alpha):
-        """Performs the necessary steps to do a move of mirror 2."""
-        # Exactly the same as notes on move_alpha_1 but with alpha2.
-        raise NotImplementedError
+    def jog_alphas_to_pixels(self, new_pixel_1, new_pixel_2):
+        """
+        Jogs each mirror until the centroid on the respective imager reaches
+        the desired pixel.
+        """
+        self._jog_alpha_to_pixel(new_pixel_1, 0)
+        self._jog_alpha_to_pixel(new_pixel_2, 1)
 
 ################################################################################
 #                                 Imager Class                                 #
