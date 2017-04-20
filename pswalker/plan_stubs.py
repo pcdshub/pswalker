f4d841ce9f1e90ad6d23dc4337d1710cbede6c79
Zachary Lentz <zlentz@slac.stanford.edu>
2017-04-19 19:55:49 -0700

    start on stepwalk

---
+++ pswalker/plan_stubs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import uuid
+
+from bluesky.plans import wait, abs_set
+
+
+def prep_img_motors(n_mot, img_motors, prev_out=True, tail_in=True):
+    """
+    Plan to prepare image motors for taking data. Moves the correct imagers in
+    and waits for them to be ready.
+
+    Parameters
+    ----------
+    n_mot: int
+        Index of the motor in img_motors that we need to take data with.
+    img_motors: list of OphydObject
+        OphydObjects to move in or out. These objects need to have .set methods
+        that accept the strings "IN" and "OUT", reacting appropriately. These
+        should be ordered by increasing distance to the source.
+    prev_out: bool, optional
+        If True, pull out imagers closer to the source than the one we need to
+        use. Default True. (True if imager blocks beam, False if you don't care)
+    tail_in: bool, optional
+        If True, put in imagers after this one, to be ready for later. If
+        False, don't touch them. We won't wait for the tail motors to move in.
+    """
+    prev_img_mot = str(uuid.uuid4())
+    for i, mot in enumerate(img_motors):
+        if i < n and prev_out:
+            yield from abs_set(mot, group=prev_img_mot, "OUT")
+        elif i == n:
+            yield from abs_set(mot, group=prev_img_mot,  "IN")
+        elif tail_in:
+            yield from abs_set(mot, "IN")
+
+    yield from wait(group=prev_img_mot)
