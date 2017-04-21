44cac4d0e31d3b42cc6ab45a6d66164ef032ca13
Zachary Lentz <zlentz@slac.stanford.edu>
2017-04-21 14:09:07 -0700

    Expect a TwoMirrorSystem to be added at some point and use it now

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,11 +6,14 @@
 # Third Party #
 ###############
 import pytest
+from lightpath import BeamPath
 
 ##########
 # Module #
 ##########
 from pswalker.examples import YAG, Mirror
+from pswalker.examples import TwoMirrorSystem
+
 
 @pytest.fixture(scope='function')
 def one_bounce_system():
@@ -22,3 +25,14 @@ def one_bounce_system():
     yag = YAG('yag', mv, 'motor', 100.0)
     return mv, yag
 
+
+@pytest.fixture(scope='function')
+def fake_path_two_bounce():
+    """
+    pswalker-compatible lightpath.BeamPath with fake objects. Pretends to be
+    the HOMS system.
+    """
+    system = TwoMirrorSystem()
+    path = BeamPath(system.mirror_1, system.mirror_2,
+                    system.yag_1, system.yag_2)
+    return path
