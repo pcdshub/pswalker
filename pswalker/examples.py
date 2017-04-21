84631d10808770d2cdbe3cfb6f41ab52f5af687b
apraSLAC <dd.apra93@gmail.com>
2017-04-21 16:09:33 -0700

    Converged on a setup that works. The remaning work is to implement 'read' for the yag such it it includes the centroids and the motor information.

--- pswalker/examples.py
+++ pswalker/examples.py
@@ -31,66 +31,82 @@ class OneMirrorSystem(object):
         self._noise_a1 = kwargs.get("noise_a1", 0)
         self._noise_x2 = kwargs.get("noise_x2", 0)
         self._noise_d2 = kwargs.get("noise_d2", 0)
-        self._fake_sleep_s =  kwargs.get("fake_sleep_s", 0)
-        self._fake_sleep_m1 = kwargs.get("fake_sleep_m1", 0)
+        self._fake_sleep_s_x =  kwargs.get("fake_sleep_s_x", 0)
+        self._fake_sleep_s_xp =  kwargs.get("fake_sleep_s_xp", 0)
+        self._fake_sleep_m1_x = kwargs.get("fake_sleep_m1_x", 0)
+        self._fake_sleep_m1_z = kwargs.get("fake_sleep_m1_z", 0)
+        self._fake_sleep_m1_alpha = kwargs.get("fake_sleep_m1_alpha", 0)
+        self._fake_sleep_y1_x = kwargs.get("fake_sleep_y1_x", 0)
+        self._fake_sleep_y1_z = kwargs.get("fake_sleep_y1_z", 0)
         self._name_s = kwargs.get("name_s", "Source")
         self._name_m1 = kwargs.get("name_m1", "Mirror 1")
         self._name_y1 = kwargs.get("name_y1", "YAG 1")
         self._pix_y1 = kwargs.get("pix_y1", (1392, 1040))
         self._size_y1 = kwargs.get("size_y1", (0.0076, 0.0062))
-        self._inverted_y1 = kwargs.get("inverted_y1", False)
+        self._invert_y1 = kwargs.get("invert_y1", False)
 
         self.source = Source(self._name_s, self._x0, self._xp0,
                              noise_x=self._noise_x0, noise_xp=self._noise_xp0,
-                             fake_sleep=self._fake_sleep_s)
+                             fake_sleep_x=self._fake_sleep_s_x,
+                             fake_sleep_xp=self._fake_sleep_s_xp)
         
         self.mirror_1 = Mirror(self._name_m1, self._x1, self._d1, self._a1,
                                noise_x=self._noise_x1, noise_alpha=self._noise_a1,
-                               fake_sleep=self._fake_sleep_m1)
+                               fake_sleep_x=self._fake_sleep_m1_x,
+                               fake_sleep_z=self._fake_sleep_m1_z,
+                               fake_sleep_alpha=self._fake_sleep_m1_alpha)
 
         self.yag_1 = YAG(self._name_y1, self._x2, self._d2, self._noise_x2,
-                         self._noise_d2, pix=self._pix_y1, size=self._size_y1)
+                         self._noise_d2, pix=self._pix_y1, size=self._size_y1,
+                         fake_sleep_x=self._fake_sleep_y1_x,
+                         fake_sleep_z=self._fake_sleep_y1_z)
         
         def calc_cent_x():
-            x = -2*self.mirror_1.alpha*self.mirror_1.z + \
-              2*self.mirror_1.alpha*self.yag_1.z - \
-              self.yag_1.z*self.source.xp + 2*self.mirror_1.x - self.source.x
-            return np.floor(self.yag_1.pix[0]/2) + \
+            x = OneBounce(self.mirror_1.read()['alpha']['value'],
+                          self.source.read()['x']['value'],
+                          self.source.read()['xp']['value'],
+                          self.mirror_1.read()['x']['value'],
+                          self.mirror_1.read()['z']['value'],
+                          self.yag_1.read()['z']['value'])
+            return np.floor(self.yag_1.pix[0]/2) + (1 - 2*self._invert_y1)* \
                 (x - self.x2)*self.yag_1.pix[0]/self.yag_1.size[0]
-            
+
         self.yag_1.cent_x = calc_cent_x
 
-class Source(Mover):
+def OneBounce(a1, x0, xp0, x1, d1, d2):
+    return -2*a1*d1 + 2*a1*d2 - d2*xp0 + 2*x1 - x0
+
+def TwoBounce(alphas, x0, xp0, x1, d1, x2, d2, d3):
+    return 2*alphas[0]*d1 - 2*alphas[0]*d3 - 2*alphas[1]*d2 + 2*alphas[1]*d3 + \
+        d3*xp0 - 2*x1 + 2*x2 + x0
+
+class Source(object):
     """
     Simulation of the photon source (simplified undulator).
     """
-    def __init__(self, name, x, xp, noise_x, noise_xp, fake_sleep=0):
+    def __init__(self, name, x, xp, noise_x, noise_xp, fake_sleep_x=0,
+                 fake_sleep_xp=0):
         self.name = name
-        self.x = x
-        self.xp = xp
- 
-        def position(**kwargs):
-            x = kwargs['x']
-            if noise_x:
-                x += np.random.uniform(-1, 1)*noise_x
-            self.x = x
-            return self.x
+        self.noise_x = noise_x
+        self.noise_xp = noise_xp
+        self.fake_sleep_x = fake_sleep_x
+        self.fake_sleep_xp = fake_sleep_xp
+        self.x = Mover('X Motor', OrderedDict(
+                [('x', lambda x: x + np.random.uniform(-1, 1)*self.noise_x),
+                 ('x_setpoint', lambda x: x)]), {'x': x})
+        self.xp = Mover('XP Motor', OrderedDict(
+                [('xp', lambda xp: xp + np.random.uniform(-1, 1)*self.noise_xp),
+                 ('xp_setpoint', lambda xp: xp)]), {'xp': xp})
+        self.motors = [self.x, self.xp]
         
-        def pointing(**kwargs):
-            xp = kwargs['xp']
-            if noise_xp:
-                xp += np.random.uniform(-1, 1)*noise_xp
-            self.xp = xp
-            return self.xp
-        # import ipdb; ipdb.set_trace()
-        super().__init__(
-            self.name, OrderedDict(
-                [('x', position),
-                ('xp', pointing),
-                ('x_setpoint', lambda **kwargs: kwargs['x']),
-                ('xp_setpoint', lambda **kwargs: kwargs['xp'])]),
-            {'x':self.x, 'xp':self.xp}, fake_sleep=fake_sleep)
+    def read(self):
+        return dict(ChainMap(*[motor.read() for motor in self.motors]))
 
+    def set(self, **kwargs):
+        for key in kwargs.keys():
+            for motor in self.motors:
+                if key in motor.read():
+                    motor.set(kwargs[key])
 
 class Mirror(object):
     """
@@ -120,20 +136,29 @@ class Mirror(object):
         Simulate moving time
     """
     def __init__(self, name, x, z, alpha, noise_x=0, noise_z=0, 
-                 noise_alpha=0, fake_sleep=0):
+                 noise_alpha=0, fake_sleep_x=0, fake_sleep_z=0,
+                 fake_sleep_alpha=0):
         self.name = name
-        self.x = Mover('X Motor', OrderedDict(
-                [('x', lambda x: x + np.random.uniform(-1, 1)*noise_x),
-                 ('x_setpoint', lambda x: x)]), {'x': x})
-        self.z = Mover('Z Motor', OrderedDict(
-                [('z', lambda z: z + np.random.uniform(-1, 1)*noise_z),
-                 ('z_setpoint', lambda z: z)]), {'z': z})
-        self.alpha = Mover('Alpha Motor', OrderedDict(
-                [('alpha', lambda alpha: alpha + np.random.uniform(-1, 1)*noise_alpha),
-                 ('alpha_setpoint', lambda alpha: alpha)]), {'alpha': alpha})
         self.noise_x = noise_x
         self.noise_z = noise_z
         self.noise_alpha = noise_alpha
+        self.fake_sleep_x = fake_sleep_x
+        self.fake_sleep_z = fake_sleep_z
+        self.fake_sleep_alpha = fake_sleep_alpha
+
+        self.x = Mover('X Motor', OrderedDict(
+                [('x', lambda x: x + np.random.uniform(-1, 1)*self.noise_x),
+                 ('x_setpoint', lambda x: x)]), {'x': x},
+                       fake_sleep=self.fake_sleep_x)
+        self.z = Mover('Z Motor', OrderedDict(
+                [('z', lambda z: z + np.random.uniform(-1, 1)*self.noise_z),
+                 ('z_setpoint', lambda z: z)]), {'z': z},
+                       fake_sleep=self.fake_sleep_z)
+        self.alpha = Mover('Alpha Motor', OrderedDict(
+                [('alpha', lambda alpha: alpha + \
+                      np.random.uniform(-1, 1)*self.noise_alpha),
+                 ('alpha_setpoint', lambda alpha: alpha)]), {'alpha': alpha},
+                           fake_sleep=self.fake_sleep_alpha)
         self.motors = [self.x, self.z, self.alpha]
 
     def read(self):
@@ -174,31 +199,29 @@ class YAG(Reader):
     noise_multiplier : float, optional
         Introduce unifrom random noise to the pixel location
     """
-    def __init__(self, name, x, z, noise_x, noise_z, **kwargs):
-        
+    def __init__(self, name, x, z, noise_x=0, noise_z=0, fake_sleep_x=0,
+                 fake_sleep_z=0, **kwargs):
+
         self.name = name
-        self._x = x
-        self.z = z
         self.noise_x = noise_x
         self.noise_z = noise_z
+        
+        self.fake_sleep_x = fake_sleep_x
+        self.fake_sleep_z = fake_sleep_z
+
+        self.x = Mover('X Motor', OrderedDict(
+                [('x', lambda x: x + np.random.uniform(-1, 1)*self.noise_x),
+                 ('x_setpoint', lambda x: x)]), {'x': x},
+                       fake_sleep=self.fake_sleep_x)
+        self.z = Mover('Z Motor', OrderedDict(
+                [('z', lambda z: z + np.random.uniform(-1, 1)*self.noise_z),
+                 ('z_setpoint', lambda z: z)]), {'z': z},
+                       fake_sleep=self.fake_sleep_z)
+        self.motors = [self.x, self.z]
 
         self.pix = kwargs.get("pix", (1392, 1040))
         self.size = kwargs.get("size", (0.0076, 0.0062))
-
-        def position(**kwargs):
-            x = kwargs['x']
-            if noise_x:
-                x += np.random.uniform(-1, 1)*noise_x
-            self._x = x
-            return self._x
         
-        def distance(**kwargs):
-            z = kwargs['z']
-            if noise_z:
-                z += np.random.uniform(-1, 1)*noise_z
-            self.z = z
-            return self.z
-               
         def cent_x():
             return np.floor(self.pix[0]/2)
         
@@ -208,22 +231,20 @@ class YAG(Reader):
         def cent():
             return (cent_x(), cent_y())
 
-        self.motor = Mover(self.name+" Motor",
-                           {'x': position,
-                            'x_setpoint':lambda **kwargs : kwargs['x']},
-                           {'x':self._x})
-
         super().__init__(self.name, {'centroid_x' : cent_x,
                                      'centroid_y' : cent_y,
                                      'centroid' : cent})
+    def read(self):
+        # TODO: Roll the centroid data into this large dict, rather than it
+        # just containing motor information
+        return dict(ChainMap(*[motor.read() for motor in self.motors]))
 
-    @property
-    def x(self):
-        return self.motor.read()['x']['value']
-    
-    @x.setter
-    def x(self, val):
-        self.motor.set(val)
+    def set(self, **kwargs):
+        for key in kwargs.keys():
+            for motor in self.motors:
+                if key in motor.read():
+                    motor.set(kwargs[key])
+
 
                                         
 if __name__ == "__main__":
