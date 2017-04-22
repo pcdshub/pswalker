0a69620a243ed4d9cc6c05f1c0ab62cff5fefa01
Zachary Lentz <zlentz@slac.stanford.edu>
2017-04-21 17:26:41 -0700

    add FakePath and make a fake homs setup with extraneous yags

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,13 +6,11 @@
 # Third Party #
 ###############
 import pytest
-from lightpath import BeamPath
 
 ##########
 # Module #
 ##########
 from pswalker.examples import YAG, Mirror
-from pswalker.examples import TwoMirrorSystem
 
 
 @pytest.fixture(scope='function')
@@ -21,9 +19,23 @@ def one_bounce_system():
     Generic single bounce system consisting one mirror with a linear
     relationship with YAG centroid
     """
-    mv  = Mirror('mirror', 0)
+    mv = Mirror('mirror', 0)
     yag = YAG('yag', mv, 'motor', 100.0)
     return mv, yag
+
+
+class FakePath:
+    def __init__(self, *devices):
+        self.devices = sorted(devices, key=lambda d: d.read()["z"]["value"])
+
+    def clear(self, *args, **kwargs):
+        for device in self.devices:
+            if device.blocking:
+                device.set("OUT")
+
+    @property
+    def blocking_devices(self):
+        return [d for d in self.devices if d.blocking]
 
 
 @pytest.fixture(scope='function')
@@ -32,7 +44,13 @@ def fake_path_two_bounce():
     pswalker-compatible lightpath.BeamPath with fake objects. Pretends to be
     the HOMS system.
     """
-    system = TwoMirrorSystem()
-    path = BeamPath(system.mirror_1, system.mirror_2,
-                    system.yag_1, system.yag_2)
+    p1h = YAG("p1h", 0, 0)
+    feem1 = Mirror("feem1", 0, 10, 0)
+    p2h = YAG("p2h", 0, 20)
+    feem2 = Mirror("feem2", 0, 30, 0)
+    p3h = YAG("p3h", 0, 40)
+    hx2_pim = YAG("hx2_pim", 0, 50)
+    um6_pim = YAG("um6_pim", 0, 60)
+    dg3_pim = YAG("dg3_pim", 0, 70)
+    path = FakePath(p1h, feem1, p2h, feem2, p3h, hx2_pim, um6_pim, dg3_pim)
     return path
