c1b0314f5314f6a0973d0db6e97bae3c21a0ebec
Zachary Lentz <zlentz@slac.stanford.edu>
2017-05-18 10:26:56 -0700

    Define an interface for iterwalk

--- pswalker/iterwalk.py
+++ pswalker/iterwalk.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+def iterwalk(detectors, motors, goals, starts=None, first_steps=1,
+             gradients=None, detector_fields='centroid_x',
+             motor_fields='alpha', tolerances=20, system=None, averages=None,
+             max_walks=None):
+    pass
