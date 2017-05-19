cfe77f297bf25bcfd97d824044d3fe4c03b95994
Zachary Lentz <zlentz@slac.stanford.edu>
2017-05-19 16:05:27 -0700

    reformat logger string

--- pswalker/__init__.py
+++ pswalker/__init__.py
@@ -5,5 +5,7 @@ __version__ = get_versions()['version']
 del get_versions
 
 logfile = os.path.join(os.path.dirname(__file__), "log.txt")
-logging.basicConfig(level=logging.DEBUG, filename=logfile)
+logging.basicConfig(level=logging.DEBUG, filename=logfile,
+                    format='%(asctime)s - %(levelname)s ' +
+                           '- %(name)s - %(message)s')
 logger = logging.getLogger(__name__)
