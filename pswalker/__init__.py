49deb6dcf189c22e82da5a131cabd512dba3d3ee
Zachary Lentz <zlentz@slac.stanford.edu>
2017-05-19 11:05:48 -0700

    change where the logfile goes

--- pswalker/__init__.py
+++ pswalker/__init__.py
@@ -4,6 +4,6 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-logfile = os.path.join(os.path.dirname(__name__), "log.txt")
+logfile = os.path.join(os.path.dirname(__file__), "log.txt")
 logging.basicConfig(level=logging.DEBUG, filename=logfile)
 logger = logging.getLogger(__name__)
