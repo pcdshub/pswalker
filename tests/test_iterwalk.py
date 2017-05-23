0f892733b7e9ed18039916eb347c9c21f954aab6
Zachary Lentz <zlentz@slac.stanford.edu>
2017-05-23 16:21:26 -0700

    reimplement test_iterwalk in a parametrized way

--- tests/test_iterwalk.py
+++ tests/test_iterwalk.py
@@ -7,10 +7,7 @@ import logging
 ###############
 import pytest
 import numpy as np
-from bluesky             import Msg, RunEngine
-from bluesky.plans       import run_wrapper
-# from bluesky.callbacks   import collector
-# from bluesky.tests.utils import MsgCollector
+from bluesky.plans import run_wrapper
 from ophyd.status import Status
 ##########
 # Module #
@@ -19,83 +16,43 @@ from pswalker.iterwalk import iterwalk
 
 TOL = 5
 logger = logging.getLogger(__name__)
-re_logger = logging.getLogger("RunEngine")
 
-def test_iterwalk_terminates_on_convergence(RE, lcls_two_bounce_system):
-    logger.debug("test_iterwalk_terminates_on_convergence")
+
+@pytest.mark.timeout(3)
+@pytest.mark.parametrize("goal1", [-300, 0, 300])
+@pytest.mark.parametrize("goal2", [-300, 0, 300])
+@pytest.mark.parametrize("first_steps", [1e-6])
+@pytest.mark.parametrize("gradients", [None])
+@pytest.mark.parametrize("tolerances", [3])
+@pytest.mark.parametrize("overshoot", [0, 0.25])
+@pytest.mark.parametrize("max_walks", [5])
+def test_iterwalk(RE, lcls_two_bounce_system,
+                  goal1, goal2, first_steps, gradients,
+                  tolerances, overshoot, max_walks):
+    logger.debug("test_iterwalk with goal1=%s, goal2=%s, first_steps=%s, " +
+                 "gradients=%s, tolerances=%s, overshoot=%.2f, max_walks=%s",
+                 goal1, goal2, first_steps, gradients, tolerances, overshoot,
+                 max_walks)
     s, m1, m2, y1, y2 = lcls_two_bounce_system
 
-    # Center pixels of yag
-    center_pix = [y1.pix[0]/2] * 2
+    goal1 += y1.pix[0]/2
+    goal2 += y2.pix[0]/2
 
-    plan = run_wrapper(iterwalk([y1, y2], [m1, m2], center_pix, starts=None,
-                                first_steps=1e-6, gradients=None,
-                                detector_fields='centroid_x', motor_fields='alpha',
-                                tolerances=TOL, system=None, averages=1,
-                                overshoot=0, max_walks=5, timeout=None))
-    RE(plan)
-    assert np.isclose(y1.read()['centroid_x']['value'], center_pix[0], atol=TOL)
-    assert np.isclose(y2.read()['centroid_x']['value'], center_pix[0], atol=TOL)
-                       
-def test_iterwalk_converges_on_same_side_goal_pixels(RE, lcls_two_bounce_system):
-    logger.debug("test_iterwalk_converges_on_same_side_goal_pixels")
-    s, m1, m2, y1, y2 = lcls_two_bounce_system
-
-    # Center pixels of yag
-    center_pix = [y1.pix[0]/2] * 2
-    # Goal pixels both to the right of center
-    goal = [p + 300 for p in center_pix]
+    goal = [goal1, goal2]
 
     plan = run_wrapper(iterwalk([y1, y2], [m1, m2], goal, starts=None,
-                                first_steps=1e-6, gradients=None,
-                                detector_fields='centroid_x', motor_fields='alpha',
-                                tolerances=TOL, system=None, averages=1,
-                                overshoot=0, max_walks=5, timeout=None))
+                                first_steps=first_steps, gradients=gradients,
+                                detector_fields='centroid_x',
+                                motor_fields='alpha',
+                                tolerances=tolerances, system=None, averages=1,
+                                overshoot=overshoot, max_walks=max_walks,
+                                timeout=None))
     RE(plan)
-    assert np.isclose(y1.read()['centroid_x']['value'], goal[0], atol=TOL)
-    assert np.isclose(y2.read()['centroid_x']['value'], goal[1], atol=TOL)
-                           
-    # Goal pixels both to the left of center
-    goal = [p - 300 for p in center_pix]
+    assert np.isclose(y1.read()['centroid_x']['value'], goal[0],
+                      atol=tolerances)
+    assert np.isclose(y2.read()['centroid_x']['value'], goal[1],
+                      atol=tolerances)
 
-    plan = run_wrapper(iterwalk([y1, y2], [m1, m2], goal, starts=None,
-                                first_steps=1e-6, gradients=None,
-                                detector_fields='centroid_x', motor_fields='alpha',
-                                tolerances=TOL, system=None, averages=1,
-                                overshoot=0, max_walks=5, timeout=None))
-    RE(plan)
-    assert np.isclose(y1.read()['centroid_x']['value'], goal[0], atol=TOL)
-    assert np.isclose(y2.read()['centroid_x']['value'], goal[1], atol=TOL)
-
-def test_iterwalk_converges_on_alternate_side_goal_pixels(RE, lcls_two_bounce_system):
-    logger.debug("test_iterwalk_converges_on_alternate_side_goal_pixels")
-    s, m1, m2, y1, y2 = lcls_two_bounce_system
-
-    # Center pixels of yag
-    center_pix = [y1.pix[0]/2] * 2
-    # Goal pixels one to the right and one to the left
-    goal = [c + p for c,p in zip(center_pix, [200, -200])]
-
-    plan = run_wrapper(iterwalk([y1, y2], [m1, m2], goal, starts=None,
-                                first_steps=1e-6, gradients=None,
-                                detector_fields='centroid_x', motor_fields='alpha',
-                                tolerances=TOL, system=None, averages=1,
-                                overshoot=0, max_walks=5, timeout=None))
-    RE(plan)
-    assert np.isclose(y1.read()['centroid_x']['value'], goal[0], atol=TOL)
-    assert np.isclose(y2.read()['centroid_x']['value'], goal[1], atol=TOL)
-                           
-    # Goal pixels one to the left and one to the right
-    goal = [c + p for c,p in zip(center_pix, [-200, 200])]
-
-    plan = run_wrapper(iterwalk([y1, y2], [m1, m2], goal, starts=None,
-                                first_steps=1e-6, gradients=None,
-                                detector_fields='centroid_x', motor_fields='alpha',
-                                tolerances=TOL, system=None, averages=1,
-                                overshoot=0, max_walks=5, timeout=None))
-    RE(plan)
-    assert np.isclose(y1.read()['centroid_x']['value'], goal[0], atol=TOL)
-    assert np.isclose(y2.read()['centroid_x']['value'], goal[1], atol=TOL)
 
 @pytest.mark.timeout(3)
 def test_iterwalk_raises_RuntimeError_on_motion_timeout(RE, lcls_two_bounce_system):
