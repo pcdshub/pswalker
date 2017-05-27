837416eef096b5fe19408959cf6bc202b62a1a7f
Zachary Lentz <zlentz@slac.stanford.edu>
2017-05-27 12:46:55 -0700

    add basic test for branching plan

--- tests/test_skywalker.py
+++ tests/test_skywalker.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from bluesky.plans import count, mv
 from pswalker.skywalker import branching_plan
+from .utils import collector
+
+
+def test_branching_plan(RE, lcls_two_bounce_system):
+    s, m1, m2, y1, y2 = lcls_two_bounce_system
+
+    reads = []
+
+    def test_plan():
+        yield from mv(m1, 0)
+        assert m1.position == 0, 'test init failed'
+
+        plan = count([m1], 10)
+
+        def branch():
+            return (yield from mv(m1, m1.position + 1))
+
+        def choice():
+            if len(reads) > m1.position:
+                return 0
+            return None
+
+        yield from branching_plan(plan, [branch], choice, branch_msg='checkpoint')
+
+    RE(test_plan, collector('alpha', reads))
+
+    assert len(reads) == 10
+    assert reads == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
