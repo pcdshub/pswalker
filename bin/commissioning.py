b1423694079486566a5f50f21cd91aa057a31dd9
Zachary Lentz <zlentz@slac.stanford.edu>
2017-05-28 16:08:24 -0700

    move commissioning to bin folder

--- bin/commissioning.py
+++ bin/commissioning.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from .plans import walk_to_pixel  # NOQA
-from .iterwalk import iterwalk  # NOQA
-from .skywalker import (homs_RE, homs_system,  # NOQA
+from pswalker.plans import walk_to_pixel  # NOQA
+from pswalker.iterwalk import iterwalk  # NOQA
+from pwsalker.skywalker import (homs_RE, homs_system,  # NOQA
                         homs_skywalker as skywalker,
                         run_homs_skywalker as run_skywalker)
-from .suspenders import PvAlarmSuspend  # NOQA
+from pswalker.suspenders import PvAlarmSuspend  # NOQA
 
 RE = homs_RE()
 system = homs_system()
