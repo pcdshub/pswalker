700fc15b8f326edc1dfab82bd43d7acc6efe163c
Zachary Lentz <zlentz@slac.stanford.edu>
2017-06-01 14:03:13 -0700

    logging rework

--- bin/commissioning.py
+++ bin/commissioning.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import logging
+import os.path
+run_dir = '/reg/g/pcds/pyps/apps/skywalker'
+if not os.path.exists(run_dir):
+    run_dir = os.path.abspath('.')
+logfile = os.path.join(run_dir, 'log.txt')
+logging.basicConfig(level=logging.DEBUG, filename=logfile,
+                    format='%(asctime)s - %(levelname)s ' +
+                           '- %(name)s - %(message)s')
 
 from bluesky.plans import run_wrapper
 from bluesky.callbacks import LiveTable
@@ -11,9 +19,11 @@ from pswalker.plans import walk_to_pixel, measure_average, measure_centroid  # N
 from pswalker.plan_stubs import recover_threshold, prep_img_motors
 from pswalker.iterwalk import iterwalk  # NOQA
 from pswalker.skywalker import (homs_RE, homs_system,  # NOQA
-                        homs_skywalker as skywalker,
-                        run_homs_skywalker as run_skywalker)
+                                homs_skywalker as skywalker,
+                                run_homs_skywalker as run_skywalker,
+                                m1h_pitch, m2h_pitch, hx2_cent_x, dg3_cent_x)
 from pswalker.suspenders import PvAlarmSuspend  # NOQA
+
 
 re_logger = logging.getLogger("RunEngine")
 
@@ -31,11 +41,6 @@ m1h.low_limit = -0.08
 m1h.high_limit = 0.13
 m2h.low_limit = -0.15
 m2h.high_limit = 0.09
-
-m1h_pitch = "MIRR:FEE1:M1H_pitch"
-m2h_pitch = "MIRR:FEE1:M2H_pitch"
-hx2_cent_x = "HX2:SB1:PIM_detector_stats2_centroid_y"
-dg3_cent_x = "HFX:DG3:PIM_detector_stats2_centroid_y"
 
 xcsdg3 = PIMMotor("XCS:DG3:PIM2")
 xcssb2 = PIMMotor("XCS:SB2:PIM6")
