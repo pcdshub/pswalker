b424c7680d03cf4b79018205d247c75861a2b479
Zachary Lentz <zlentz@slac.stanford.edu>
2017-06-01 13:18:52 -0700

    add xrtm2 and xcs-line pims to commissioning script

--- bin/commissioning.py
+++ bin/commissioning.py
@@ -5,8 +5,10 @@ import logging
 from bluesky.plans import run_wrapper
 from bluesky.callbacks import LiveTable
 
+from pcdsdevices.epics.pim import PIMMotor
+
 from pswalker.plans import walk_to_pixel, measure_average, measure_centroid  # NOQA
-from pswalker.plan_stubs import recover_threshold
+from pswalker.plan_stubs import recover_threshold, prep_img_motors
 from pswalker.iterwalk import iterwalk  # NOQA
 from pswalker.skywalker import (homs_RE, homs_system,  # NOQA
                         homs_skywalker as skywalker,
@@ -21,6 +23,7 @@ RE.msg_hook = re_logger.debug
 system = homs_system()
 m1h = system['m1h']
 m2h = system['m2h']
+xrtm2 = system['xrtm2']
 hx2 = system['hx2']
 dg3 = system['dg3']
 
@@ -33,3 +36,6 @@ m1h_pitch = "MIRR:FEE1:M1H_pitch"
 m2h_pitch = "MIRR:FEE1:M2H_pitch"
 hx2_cent_x = "HX2:SB1:PIM_detector_stats2_centroid_y"
 dg3_cent_x = "HFX:DG3:PIM_detector_stats2_centroid_y"
+
+xcsdg3 = PIMMotor("XCS:DG3:PIM2")
+xcssb2 = PIMMotor("XCS:SB2:PIM6")
