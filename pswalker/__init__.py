af1e368ebc8e47e485c2905e0973628e8b852feb
Zachary Lentz <zlentz@slac.stanford.edu>
2017-06-01 13:29:27 -0700

    only set up pswalker logger if one is not set up

--- pswalker/__init__.py
+++ pswalker/__init__.py
@@ -4,8 +4,9 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-logfile = os.path.join(os.path.dirname(__file__), "log.txt")
-logging.basicConfig(level=logging.DEBUG, filename=logfile,
-                    format='%(asctime)s - %(levelname)s ' +
-                           '- %(name)s - %(message)s')
+if len(logging.root.handlers) == 0:
+    logfile = os.path.join(os.path.dirname(__file__), "log.txt")
+    logging.basicConfig(level=logging.DEBUG, filename=logfile,
+                        format='%(asctime)s - %(levelname)s ' +
+                               '- %(name)s - %(message)s')
 logger = logging.getLogger(__name__)
