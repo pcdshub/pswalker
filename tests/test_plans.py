ea0cf34b46f55ad93c9076336ed9fcad072feafb
Zachary Lentz <zlentz@slac.stanford.edu>
2017-06-10 14:50:37 -0700

    add a test for measuring the whole system, adjust test keys for example obj names

--- tests/test_plans.py
+++ tests/test_plans.py
@@ -28,8 +28,8 @@ def test_measure_average(RE, one_bounce_system):
     #Fake event storage 
     centroids = []
     readbacks = []
-    col_c = collector('centroid_x', centroids)
-    col_r = collector('alpha',    readbacks)
+    col_c = collector(det.name + '_centroid_x', centroids)
+    col_r = collector(mot.name + '_alpha',    readbacks)
     #Run plan
     RE(run_wrapper(measure_average([det, mot],
                                    ['centroid_x','alpha'],
@@ -57,6 +57,44 @@ def test_measure_average(RE, one_bounce_system):
                                        ['centroid_x','alpha'],
                                        delay=[0.1], num=3)))
 
+def test_measure_average_system(RE, lcls_two_bounce_system):
+    logger.debug("test_measure_average_system")
+    _, m1, m2, y1, y2 = lcls_two_bounce_system
+
+    centroids = []
+    readbacks = []
+    col_c = collector(y1.name + '_centroid_x', centroids)
+    col_r = collector(m1.name + '_alpha',    readbacks)
+
+    RE(run_wrapper(measure_average([y1, m1, y2, m2],
+                                   ['centroid_x','alpha'],
+                                   delay=0.1, num=5)),
+       subs={'event':[col_c, col_r]})
+
+    assert centroids == [y1.cent_x()] * 5
+    assert readbacks == [m1.position] * 5
+
+    # RE.msg_hook is a message collector
+    m1_reads = 0
+    m2_reads = 0
+    y1_reads = 0
+    y2_reads = 0
+    saves = 0
+    for msg in RE.msg_hook.msgs:
+        if msg.command == 'read':
+            if msg.obj == m1:
+                m1_reads += 1
+            if msg.obj == m2:
+                m2_reads += 1
+            if msg.obj == y1:
+                y1_reads += 1
+            if msg.obj == y2:
+                y2_reads += 1
+        if msg.command == 'save':
+            saves += 1
+    assert saves > 0
+    assert all(map(lambda x: x == saves,
+                   [m1_reads, m2_reads, y1_reads, y2_reads]))
 
 def test_measure_centroid(RE, one_bounce_system):
     logger.debug("test_measure_centroid")
@@ -64,7 +102,7 @@ def test_measure_centroid(RE, one_bounce_system):
     _, mot, det = one_bounce_system
     #Fake event storage 
     centroids = []
-    col_c = collector('centroid_x', centroids)
+    col_c = collector(det.name + '_centroid_x', centroids)
     #Run plan
     # assert 0
     RE(run_wrapper(measure_centroid(det, average=5, target_field='centroid_x')),
@@ -82,11 +120,11 @@ def test_walk_to_pixel(RE, one_bounce_system):
                                      target_fields=['centroid_x', 'alpha'],
                                      max_steps=3))
     RE(plan)
-    assert np.isclose(det.read()['centroid_x']['value'], 200, atol=10)
+    assert np.isclose(det.read()[det.name + '_centroid_x']['value'], 200, atol=10)
     #Walk to pixel using intial guess at gradient
     plan = run_wrapper(walk_to_pixel(det, mot, 200, 0, gradient=200,
                                      tolerance=10, average=None,
                                      target_fields=['centroid_x', 'alpha'],
                                      max_steps=3))
     RE(plan)
-    assert np.isclose(det.read()['centroid_x']['value'], 200, atol=10)
+    assert np.isclose(det.read()[det.name + '_centroid_x']['value'], 200, atol=10)
