67802ad8c7d297ac0ac6db87946ab6921fe5edc5
apra93 <dd.apra93@gmail.com>
2017-06-13 16:49:53 -0700

    Removed all classes that were migrated to pcdsdevices. Subclassing only what we need just to add the yag patching properties.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 ############
 # Standard #
 ############
 import time
-import subprocess
 from collections import OrderedDict
 ###############
 # Third Party #
@@ -16,52 +17,7 @@ import numpy as np
 #                                _calc_cent_x, _m1_calc_cent_x,
 #                                _m1_m2_calc_cent_x)
 
-from pswalker.examples import (OMMotor, OffsetMirror, PluginBase, StatsPlugin,
-                               CamBase, PulnixCam, DetectorBase, PulnixDetector,
-                               PIMPulnixDetector, PIMMotor, PIM)
-
-# OMMotor Tests
-
-def test_OMMotor_instantiates():
-    assert(OMMotor("TEST"))
-
-def test_OMMotor_runs_ophyd_functions():
-    ommotor = OMMotor("TEST")
-    assert(isinstance(ommotor.read(), OrderedDict))
-    assert(isinstance(ommotor.describe(), OrderedDict))
-    assert(isinstance(ommotor.describe_configuration(), OrderedDict))
-    assert(isinstance(ommotor.read_configuration(), OrderedDict))
-
-def test_OMMotor_moves_properly():
-    ommotor = OMMotor("TEST")
-    status = ommotor.move(10)
-    assert(ommotor.position == 10)
-    assert(status.success)
-    
-def test_OMMotor_velocity_move_time():
-    ommotor = OMMotor("TEST")
-    diff = 1
-    next_pos = ommotor.position + diff
-    ommotor.velocity.put(0.5)    
-    t0 = time.time()
-    status = ommotor.move(next_pos)
-    t1 = time.time() - t0
-    assert(np.isclose(t1, diff/ommotor.velocity.value  + 0.1, rtol=0.1))
-    assert(ommotor.position == next_pos)
-    assert(status.success)
-
-def test_OMMotor_fake_sleep_move_time():
-    ommotor = OMMotor("TEST")
-    diff = 1
-    next_pos = ommotor.position + diff
-    ommotor.velocity.put(0)    
-    ommotor.fake_sleep = 1
-    t0 = time.time()
-    status = ommotor.move(next_pos)
-    t1 = time.time() - t0
-    assert(np.isclose(t1, ommotor.fake_sleep + 0.1, rtol=0.1))
-    assert(ommotor.position == next_pos)
-    assert(status.success)
+from pswalker.examples import (OffsetMirror, PIM)
 
 # OffsetMirror tests
 
@@ -83,12 +39,6 @@ def test_OffsetMirror_runs_ophyd_functions():
     assert(isinstance(om.describe_configuration(), OrderedDict))
     assert(isinstance(om.read_configuration(), OrderedDict))
 
-def test_OffsetMirror_move_method():
-    om = OffsetMirror("TEST")
-    om.move(10)
-    assert(om.position == 10)
-    assert(om.pitch.position == 10)
-    
 def test_OffsetMirror_yag_patch_properties():
     om = OffsetMirror("TEST", x=10, y=15, z=20, alpha=1)
     assert(om._x == 10)
@@ -103,122 +53,6 @@ def test_OffsetMirror_yag_patch_properties():
     assert(om._y == 100)
     assert(om._z == 125)
     assert(om._alpha == 50)
-
-# PluginBase Tests
-
-def test_PluginBase_instantiates():
-    assert(PluginBase("TEST"))
-
-def test_PluginBase_runs_ophyd_functions():
-    plugin = PluginBase("TEST")
-    assert(isinstance(plugin.read(), OrderedDict))
-    assert(isinstance(plugin.describe(), OrderedDict))
-    assert(isinstance(plugin.describe_configuration(), OrderedDict))
-    assert(isinstance(plugin.read_configuration(), OrderedDict))
-
-# StatsPlugin Tests
-
-def test_StatsPlugin_instantiates():
-    assert(StatsPlugin("TEST"))
-
-def test_StatsPlugin_runs_ophyd_functions():
-    plugin = StatsPlugin("TEST")
-    assert(isinstance(plugin.read(), OrderedDict))
-    assert(isinstance(plugin.describe(), OrderedDict))
-    assert(isinstance(plugin.describe_configuration(), OrderedDict))
-    assert(isinstance(plugin.read_configuration(), OrderedDict))
-
-# CamBase Tests
-
-def test_CamBase_instantiates():
-    assert(CamBase("TEST"))
-
-def test_CamBase_runs_ophyd_functions():
-    cam = CamBase("TEST")
-    assert(isinstance(cam.read(), OrderedDict))
-    assert(isinstance(cam.describe(), OrderedDict))
-    assert(isinstance(cam.describe_configuration(), OrderedDict))
-    assert(isinstance(cam.read_configuration(), OrderedDict))
-
-# PulnixCam Tests
-
-def test_PulnixCam_instantiates():
-    assert(PulnixCam("TEST"))
-
-def test_PulnixCam_runs_ophyd_functions():
-    pulnix = PulnixCam("TEST")
-    assert(isinstance(pulnix.read(), OrderedDict))
-    assert(isinstance(pulnix.describe(), OrderedDict))
-    assert(isinstance(pulnix.describe_configuration(), OrderedDict))
-    assert(isinstance(pulnix.read_configuration(), OrderedDict))
-
-# DetectorBase Tests
-
-def test_DetectorBase_instantiates():
-    assert(DetectorBase("TEST"))
-
-def test_DetectorBase_runs_ophyd_functions():
-    det = DetectorBase("TEST")
-    assert(isinstance(det.read(), OrderedDict))
-    assert(isinstance(det.describe(), OrderedDict))
-    assert(isinstance(det.describe_configuration(), OrderedDict))
-    assert(isinstance(det.read_configuration(), OrderedDict))
-
-# PulnixDetector Tests
-
-def test_PulnixDetector_instantiates():
-    assert(PulnixDetector("TEST"))
-
-def test_PulnixDetector_runs_ophyd_functions():
-    pulnix_det = PulnixDetector("TEST")
-    assert(isinstance(pulnix_det.read(), OrderedDict))
-    assert(isinstance(pulnix_det.describe(), OrderedDict))
-    assert(isinstance(pulnix_det.describe_configuration(), OrderedDict))
-    assert(isinstance(pulnix_det.read_configuration(), OrderedDict))
-
-def test_PulnixDetector_cam_component_reads():
-    pulnix_det = PulnixDetector("TEST")
-    assert(isinstance(pulnix_det.cam.read(), OrderedDict))
-
-# PIMPulnixDetector Tests
-
-def test_PIMPulnixDetector_instantiates():
-    assert(PIMPulnixDetector("TEST"))
-
-def test_PIMPulnixDetector_runs_ophyd_functions():
-    pim_pulnix_det = PIMPulnixDetector("TEST")
-    assert(isinstance(pim_pulnix_det.read(), OrderedDict))
-    assert(isinstance(pim_pulnix_det.describe(), OrderedDict))
-    assert(isinstance(pim_pulnix_det.describe_configuration(), OrderedDict))
-    assert(isinstance(pim_pulnix_det.read_configuration(), OrderedDict))
-
-def test_PIMPulnixDetector_stats_plugin_reads():
-    pim_pulnix_det = PIMPulnixDetector("TEST")
-    assert(isinstance(pim_pulnix_det.stats2.read(), OrderedDict))
-
-# PIMMotor Tests
-
-def test_PIMMotor_instantiates():
-    assert(PIMMotor("TEST"))
-
-def test_PIMMotor_runs_ophyd_functions():
-    pmotor = PIMMotor("TEST")
-    assert(isinstance(pmotor.read(), OrderedDict))
-    assert(isinstance(pmotor.describe(), OrderedDict))
-    assert(isinstance(pmotor.describe_configuration(), OrderedDict))
-    assert(isinstance(pmotor.read_configuration(), OrderedDict))
-
-def test_PIMMotor_transitions_states_correctly():
-    pmotor = PIMMotor("TEST")
-    status = pmotor.move("OUT")
-    assert(pmotor.position == "OUT")
-    assert(status.success)
-    status = pmotor.move("IN")
-    assert(pmotor.position == "IN")
-    assert(status.success)
-    status = pmotor.move("DIODE")
-    assert(pmotor.position == "DIODE")
-    assert(status.success)
 
 # PIM Tests
 
