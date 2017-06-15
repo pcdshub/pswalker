ac2f5db8db66479c05866930b64fd332d7b673a5
Zachary Lentz <zlentz@slac.stanford.edu>
2017-06-15 14:06:39 -0700

    switch iterwalk to explicitly look for a RecoverDone exception and handle the case by restarting the step

--- pswalker/utils/exceptions.py
+++ pswalker/utils/exceptions.py
@@ -69,3 +69,10 @@ class MonitorException(PSWalkerException):
 	"""Base ModelBuild exception class."""
 	pass
 
+################################################################################
+#                  Other Exceptions (no reasonable category)                   #
+################################################################################
+
+class RecoverDone(PSWalkerException):
+    """Raised when a recovery has occured so the encompassing plan can react"""
+    pass
