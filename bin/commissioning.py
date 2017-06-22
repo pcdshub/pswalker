4bb21e437471cd7ca24716553f2fc375c99f5764
Zachary Lentz <zlentz@slac.stanford.edu>
2017-06-22 14:33:15 -0700

    remove empty import

--- bin/commissioning.py
+++ bin/commissioning.py
@@ -13,20 +13,22 @@ from pswalker.plan_stubs import recover_threshold, prep_img_motors
 from pswalker.iterwalk import iterwalk  # NOQA
 from pswalker.skywalker import (homs_RE, homs_system,  # NOQA
                                 homs_skywalker as skywalker,
-                                run_homs_skywalker as run_skywalker,
                                 pitch_key, cent_x_key,
                                 m1h_pitch, m2h_pitch, hx2_cent_x, dg3_cent_x,
                                 make_homs_recover)
 from pswalker.suspenders import PvAlarmSuspend  # NOQA
+from pswalker.callbacks import PositionSaving
 
 run_dir = '/reg/g/pcds/pyps/apps/skywalker'
 if not os.path.exists(run_dir):
     run_dir = os.path.abspath('.')
 logfile = os.path.join(run_dir, 'log.txt')
+
+#strm = logging.StreamHandler()
 logging.basicConfig(level=logging.DEBUG, filename=logfile,
                     format='%(asctime)s - %(levelname)s ' +
                            '- %(name)s - %(message)s')
-
+#logging.addHandler(strm, level=logging.INFO)
 RE = homs_RE()
 RE.verbose = True # Enables internal RE logging
 
@@ -36,6 +38,7 @@ m1h2 = system['m1h2']
 m2h = system['m2h']
 m2h2 = system['m2h2']
 xrtm2 = system['xrtm2']
+xrtm22 = system['xrtm22']
 hx2 = system['hx2']
 dg3 = system['dg3']
 
