9015068205e56aa1cb2460524a0d330879387c7f
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-06-23 14:57:42 -0700

    ENH: Added tests for new walk_to_pixel

--- tests/test_plans.py
+++ tests/test_plans.py
@@ -6,18 +6,61 @@ import logging
 ###############
 # Third Party #
 ###############
+import lmfit
 import pytest
 import numpy as np
 from bluesky             import Msg, RunEngine
-from bluesky.plans       import run_wrapper
+from bluesky.plans       import mv, run_wrapper
+from bluesky.examples    import det, motor, Mover, Reader
 
 ##########
 # Module #
 ##########
-from pswalker.plans import measure_average, measure_centroid, walk_to_pixel
+from pswalker.plans import measure, measure_average, measure_centroid
+from pswalker.plans import walk_to_pixel, fitwalk
+from pswalker.callbacks import LiveBuild, LinearFit
 from .utils import collector
 
 logger = logging.getLogger(__name__)
+
+class ParabolicFit(LiveBuild):
+
+
+    def __init__(self, y, x, average=1):
+
+        #Parabola function
+        def parabola(x, a0):
+            return a0*(x**2)
+
+        #Create model
+        model = lmfit.Model(parabola,
+                            independent_vars = ['x'],
+                            missing='drop')
+
+        #Initialize build
+        super().__init__(model, y, independent_vars = {'x' : x},
+                         init_guess={'a0': 1}, update_every=1,
+                         average=average)
+
+    def eval(self, x=0., **kwargs):
+        #Check result
+        super().eval(x)
+
+        #Structure input and add past result
+        kwargs = {'x' : np.asarray(x)}
+        kwargs.update(self.result.values)
+
+        #Return prediction
+        return self.result.model.eval(**kwargs)
+
+
+    def backsolve(self, target, **kwargs):
+        #Make sure we have a fit
+        super().backsolve(target, **kwargs)
+        #Gather line information
+        a0 = self.result.values['a0']
+        #Return x position
+        return {'x' : np.sqrt(target/a0)}
 
 
 def test_measure_average(RE, one_bounce_system):
@@ -56,6 +99,7 @@ def test_measure_average(RE, one_bounce_system):
         RE(run_wrapper(measure_average([det, mot],
                                        ['detector_stats2_centroid_x','pitch'],
                                        delay=[0.1], num=3)))
+
 
 def test_measure_average_system(RE, lcls_two_bounce_system):
     logger.debug("test_measure_average_system")
@@ -96,6 +140,7 @@ def test_measure_average_system(RE, lcls_two_bounce_system):
     assert all(map(lambda x: x == saves,
                    [m1_reads, m2_reads, y1_reads, y2_reads]))
 
+
 def test_measure_centroid(RE, one_bounce_system):
     logger.debug("test_measure_centroid")
     #Load motor and yag
@@ -115,7 +160,41 @@ def test_measure_centroid(RE, one_bounce_system):
 def test_walk_to_pixel(RE, one_bounce_system):
     logger.debug("test_walk_to_pixel")
     _, mot, det = one_bounce_system
-    #Walk to the pixel using dumb first step
+    
+    ##########################
+    # Test on simple devices #
+    ##########################
+    simple_motor = Mover('motor', {'motor' : lambda x : x}, {'x' :0})
+    simple_det   = Reader('det',
+                   {'det' : lambda : 5*simple_motor.read()['motor']['value'] + 2})
+
+    #Naive step
+    plan = run_wrapper(walk_to_pixel(simple_det, simple_motor, 200, 0, first_step=1e-6,
+                                     tolerance=10, average=None,
+                                     target_fields=['det', 'motor'],
+                                     max_steps=3))
+    RE(plan)
+    assert np.isclose(simple_det.read()['det']['value'], 200, atol=1)
+
+    simple_motor.set(0.)
+
+    #Gradient
+    simple_motor = Mover('motor', {'motor' : lambda x : x}, {'x' :0})
+    simple_det   = Reader('det',
+                   {'det' : lambda : 5*simple_motor.read()['motor']['value'] + 2})
+    plan = run_wrapper(walk_to_pixel(simple_det, simple_motor, 200, 0,
+                                     gradient=1.6842e+06,
+                                     tolerance=10, average=None,
+                                     target_fields=['det', 'motor'],
+                                     max_steps=3))
+    RE(plan)
+
+    assert np.isclose(simple_det.read()['det']['value'], 200, atol=1)
+
+    ##########################
+    # Test on full model #
+    ##########################
+    #Naive step
     plan = run_wrapper(walk_to_pixel(det, mot, 200, 0, first_step=1e-6,
                                      tolerance=10, average=None,
                                      target_fields=['detector_stats2_centroid_x', 
@@ -123,9 +202,12 @@ def test_walk_to_pixel(RE, one_bounce_system):
     RE(plan)
     assert np.isclose(det.read()[det.name + 
                                  '_detector_stats2_centroid_x']['value'], 200, 
-                      atol=10)
-    #Walk to pixel using intial guess at gradient
-    plan = run_wrapper(walk_to_pixel(det, mot, 200, 0, gradient=200,
+                      atol=1)
+
+    mot.set(0.)
+
+    #Gradient
+    plan = run_wrapper(walk_to_pixel(det, mot, 200, 0, gradient=1.6842e+06,
                                      tolerance=10, average=None,
                                      target_fields=['detector_stats2_centroid_x', 
                                                     'pitch'], max_steps=3))
@@ -133,3 +215,64 @@ def test_walk_to_pixel(RE, one_bounce_system):
     assert np.isclose(det.read()[det.name + 
                                  '_detector_stats2_centroid_x']['value'], 200, 
                       atol=10)
+
+def test_measure(RE):
+    #Simplest implementation
+    plan = run_wrapper(measure([det,motor], num=5, delay=0.01))
+
+    #Fake callback storage
+    shots = list()
+    cb    = collector('det', shots)
+
+    #Run simple
+    RE(plan, subs={'event' : cb})
+    assert shots == [1.0, 1.0, 1.0, 1.0, 1.0]
+
+
+    #Create counting detector
+    index = 0
+    def count():
+        nonlocal index
+        index += 1
+        return index
+
+    counter = Reader('det', {'intensity' : count})
+
+    #Filtered implementation
+    plan = run_wrapper(measure([counter],
+                       filters = {'intensity' : lambda x : x > 2},
+                       #num=5, delay=[0.01, 0.02, 0.03, 0.04]))
+                        num=5))
+    #Fake callback storage
+    shots = list()
+    cb    = collector('intensity', shots)
+
+    #Run filtered
+    RE(plan, subs={'event' : cb})
+    assert shots == [1, 3, 4, 5, 6, 7] #2 is skipped, because read is called
+                                       #by `describe`, which is called by RE
+                                       #after first read
+
+def test_fitwalk(RE):
+    #Create simulated devices
+    motor = Mover('motor', {'motor' : lambda x : x}, {'x' :0})
+    det   = Reader('det',
+                   {'centroid' : lambda : 5*motor.read()['motor']['value'] + 2})
+
+    #Assemble linear fitting callback
+    linear = LinearFit('centroid', 'motor', average=1)
+
+    #Assemble parabolic fitting callback
+    parabola = ParabolicFit('centroid', 'motor', average=1)
+
+    #Create plan
+    walk = fitwalk([det], motor, [parabola, linear], 89.4,
+                   average=1, tolerance = 0.5 , naive_step=mv(motor, 1))
+
+    #Call with RunEngine
+    RE(run_wrapper(walk))
+
+    #Check we hit our target
+    assert np.isclose(det.read()['centroid']['value'], 89.4, 0.5)
+
+
