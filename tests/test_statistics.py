66f73182e352e07f501f8eeffacd86a63836084a
apra93 <dd.apra93@gmail.com>
2017-08-07 19:12:05 -0700

    ENH: Added some stats from the stats plugin. Made things slightly nicer.

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -8,20 +8,15 @@ import logging
 ###############
 import pytest
 import numpy as np
-from bluesky             import Msg, RunEngine
-from bluesky.plans       import mv, run_wrapper
+from bluesky.plans import run_wrapper
 
 ##########
 # Module #
 ##########
 from pswalker.statistics import beam_statistics
 
-TOL = 5
 logger = logging.getLogger(__name__)
 
-tmo = 10
-
-@pytest.mark.timeout(tmo)
 @pytest.mark.parametrize("resize", [1.0])
 @pytest.mark.parametrize("kernel", [(9,9)])
 @pytest.mark.parametrize("filter_kernel", [(9,9)])
@@ -30,22 +25,29 @@ tmo = 10
 @pytest.mark.parametrize("min_area", [100])
 @pytest.mark.parametrize("num", [5])
 @pytest.mark.parametrize("thresh_factor", [3])
+@pytest.mark.parametrize("delay", [0])
 def test_beam_statistics(RE, resize, kernel, uint_mode, thresh_mode, min_area,
-                         thresh_factor, filter_kernel, num,
+                         thresh_factor, filter_kernel, num, delay,
                          lcls_two_bounce_system):
-    s, m1, m2, y1, y2 = lcls_two_bounce_system    
+    _, _, _, y1, y2 = lcls_two_bounce_system
     array_str = "image1.array_data"
     size_str = "image1.array_size"
 
     def test_plan():
-        plan = beam_statistics(
-            [y1, y2], array_field=array_str, size_field=size_str, averages=num,
+        stats = yield from beam_statistics(
+            [y1, y2], array_field=array_str, size_field=size_str, num=num,
             kernel=kernel, resize=resize, uint_mode=uint_mode,
             thresh_factor=thresh_factor, filter_kernel=filter_kernel,
-            thresh_mode=thresh_mode)
-        stats = yield from plan
+            thresh_mode=thresh_mode, md="all", image_delay=delay)
+
+        for _, det in stats.items():
+            for key, val in det.items():
+                if key == "md":
+                    continue
+                assert(not np.isnan(val) or not np.isinf(val) or not None)
+
+
         import IPython; IPython.embed()
 
-    
     RE(run_wrapper(test_plan()))
     
