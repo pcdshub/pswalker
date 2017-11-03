b50b133dc497c59cef09c53adc0176c2e41c7904
Zachary Lentz <zlentz@slac.stanford.edu>
2017-11-03 15:03:27 -0700

    TST: Redo logging my way

--- run_tests.py
+++ run_tests.py
@@ -1,16 +1,42 @@
 #!/usr/bin/env python
 import sys
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 import pytest
 
 if __name__ == '__main__':
-    #Show output results from every test function
-    #Show the message output for skipped and expected failures
+    # Show output results from every test function
+    # Show the message output for skipped and expected failures
     args = ['-v', '-vrxs']
 
-    #Add extra arguments
-    if len(sys.argv) >1:
+    # Add extra arguments
+    if len(sys.argv) > 1:
         args.extend(sys.argv[1:])
 
-    print('pytest arguments: {}'.format(args))
+    txt = 'pytest arguments: {}'.format(args)
+    print(txt)
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+    log_filename = os.path.join(os.path.dirname(__file__), 'debug.log')
+    if os.path.isfile(log_filename):
+        do_rollover = True
+    else:
+        do_rollover = False
+    handler = RotatingFileHandler(log_filename, backupCount=9)
+    if do_rollover:
+        handler.doRollover()
+    formatter = logging.Formatter(fmt=('%(asctime)s.%(msecs)03d '
+                                       '%(module)-13s '
+                                       '%(levelname)-8s '
+                                       '%(threadName)-10s '
+                                       '%(message)s'),
+                                  datefmt='%H:%M:%S')
+    handler.setFormatter(formatter)
+    root_logger.addHandler(handler)
+
+    logger = logging.getLogger(__name__)
+    logger.info(txt)
 
     sys.exit(pytest.main(args))
--- tests/conftest.py
+++ tests/conftest.py
@@ -30,31 +30,6 @@ from .utils import SlowSoftPositioner, MotorSignal, SlowOffsetMirror
 # Logging Setup #
 #################
 
-#Default logfile
-logfile = os.path.join(os.path.dirname(__file__), "log.txt")
-#Enable the logging level to be set from the command line
-def pytest_addoption(parser):
-    parser.addoption("--log", action="store", default="DEBUG",
-                     help="Set the level of the log")
-    parser.addoption("--logfile", action="store", default=logfile,
-                     help="Write the log output to specified file path")
-
-#Create a fixture to automatically instantiate logging setup
-@pytest.fixture(scope='session', autouse=True)
-def set_level(pytestconfig):
-    #Read user input logging level
-    log_level = getattr(logging, pytestconfig.getoption('--log'), None)
-
-    #Report invalid logging level
-    if not isinstance(log_level, int):
-        raise ValueError("Invalid log level : {}".format(log_level))
-
-    #Create basic configuration
-    logging.basicConfig(level=log_level,
-                        filename=pytestconfig.getoption('--logfile'),
-                        format='%(asctime)s - %(levelname)s ' +
-                               '- %(name)s - %(message)s')
-
 logger = logging.getLogger(__name__)
 logger.info("pytest start")
 run_engine_logger = logging.getLogger("RunEngine")
